ac73fcac3e03a39d081f25dc68e072112fd3912d
Daniel Hillwig <hillwig@open4business.de>
2021-09-21 15:49:02 +0200

    chore: minor changes

--- src/test/java/de/caritas/cob/uploadservice/api/service/LiveEventNotificationServiceTest.java
+++ src/test/java/de/caritas/cob/uploadservice/api/service/LiveEventNotificationServiceTest.java
@@ -7,7 +7,6 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.when;
 import static org.powermock.reflect.Whitebox.setInternalState;
 
 import de.caritas.cob.uploadservice.api.service.helper.ServiceHelper;
