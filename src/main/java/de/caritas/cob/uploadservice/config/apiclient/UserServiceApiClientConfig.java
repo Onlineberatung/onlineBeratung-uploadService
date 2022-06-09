574c21282f582c56aa967d22a14839890df1b4fd
tkuzynow <tomasz.kuzynowski@gmail.com>
2022-06-09 09:58:42 +0200

    fix: switch to usercontroller api call instead of resttemplate.exchange

--- src/main/java/de/caritas/cob/uploadservice/config/apiclient/UserServiceApiClientConfig.java
+++ src/main/java/de/caritas/cob/uploadservice/config/apiclient/UserServiceApiClientConfig.java
@@ -1,9 +1,9 @@
 package de.caritas.cob.uploadservice.config.apiclient;
 
 import de.caritas.cob.uploadservice.userservice.generated.ApiClient;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,12 +14,12 @@ public class UserServiceApiClientConfig {
   private String userServiceApiUrl;
 
   @Bean
-  public de.caritas.cob.uploadservice.userservice.generated.web.UserControllerApi userControllerApi(ApiClient apiClient) {
+  public de.caritas.cob.uploadservice.userservice.generated.web.UserControllerApi userControllerApi(@Qualifier("user") ApiClient apiClient) {
     return new de.caritas.cob.uploadservice.userservice.generated.web.UserControllerApi(apiClient);
   }
 
   @Bean
-  @Primary
+  @Qualifier("user")
   public ApiClient userApiClient(RestTemplate restTemplate) {
     return new ApiClient(restTemplate).setBasePath(this.userServiceApiUrl);
   }
